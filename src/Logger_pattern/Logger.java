7fbc3eabc45ae33dcd56102bcce1c55584da8342
esimb <230723@astanait.edu.kz>
2024-10-02 15:41:43 +0300

    Logger_pattern

---
+++ src/Logger_pattern/Logger.java
@@ -0,0 +1,30 @@
+package loggerpattern;
+
+public class Logger {
+
+    //step 1
+    private static Logger instance;
+
+
+    //step2
+    private Logger(){
+
+
+    }
+
+    //step 3
+    public static Logger getInstance(){
+        if (instance==null){
+            instance = new Logger();
+        }
+        return instance;
+
+    }
+
+    //step 4
+    public static void logmessage(){
+        System.out.println("Hello World");
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.logging.Logger;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        loggerpattern.Logger logger = loggerpattern.Logger.getInstance();
+
+        logger.logmessage();
+
+
+    }
+}
